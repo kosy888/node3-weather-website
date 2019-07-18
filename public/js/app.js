c38e45c0a5cb3dc7a21b59697ae1e8c882b0f543
kosililian <ireoforliliank@gmail.com>
2019-07-18 05:42:30 +0100

    Init commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,31 @@
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+ console.log('Client side javascript file is loaded!')
 
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+ const weatherForm = document.querySelector('form')
+ const search = document.querySelector('input')
+ const messageOne = document.querySelector('#message-1')
+ const messageTwo = document.querySelector('#message-2')
 
-    const location = search.value
+ weatherForm.addEventListener('submit', (e) => {
+     e.preventDefault()
 
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
+     const location = search.value
 
-    fetch('/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-            } else {
+     messageOne.textContent = 'Loading...'
+     messageTwo.textContent = ''
+
+     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+         response.json().then((data) => {
+             if (data.error) {
+                 messageOne.textContent = data.error
+             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
-            }
-        })
-    })
-})
+             }
+         })
+     })
+
+ })
+   
+
+     
+ 
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,6 @@
 const request = require('request')
-
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoia2Fvc2lsaWxpYW4iLCJhIjoiY2p5OTNlNmpnMDAwcTNoczA4aTFnaDN0YSJ9.QILC_CHp_cobyHMiqCGb-A'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -13,9 +12,11 @@ const geocode = (address, callback) => {
                 latitude: body.features[0].center[1],
                 longitude: body.features[0].center[0],
                 location: body.features[0].place_name
+
             })
         }
     })
+    
 }
 
 module.exports = geocode
